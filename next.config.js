c2dc8e523c825f203c9759aa6b605da7c411a130
Brandon Davis <neusfear@gmail.com>
2022-06-16 17:58:38 -0500

    Make the wasm file go where it's supposed to

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const CopyPlugin = require("copy-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
   webpack5: true,
   webpack: (config) => {
+    
     config.resolve.fallback = { fs: false };
+
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: "node_modules/rhino3dm/rhino3dm.wasm", to: "static/chunks/pages/"}
+        ]
+      })
+    )
+
     return config;
   },
 }
